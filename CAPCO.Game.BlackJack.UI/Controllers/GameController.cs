2b47f0fc1f9c3d08bde4c59bbb59f931ece79c7f
Daniel Pereira da Silva <AVA092@einstein.br>
2018-08-18 13:50:49 -0300

    Commit de estrutura

---
+++ CAPCO.Game.BlackJack.UI/Controllers/GameController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CAPCO.Game.BlackJack.UI.Controllers
+{
+    [Route("api/GameController")]
+    [ApiController]
+    public class GameController : ControllerBase
+    {
+        [HttpGet("NewGame")]
+        public IActionResult NewGame()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ CAPCO.Game.BlackJack.UI/Startup.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace CAPCO.Game.BlackJack.UI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration) => Configuration = configuration;
+
+        public string SystemVersion { get; set; }
+        public string SystemName { get; set; }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            SystemVersion = "Versão 0.0.1";
+
+            SystemName = "CAPCO - Game - BlackJack";
+
+            services.AddSwaggerGen(c => c.SwaggerDoc(SystemVersion, new Info { Title = SystemName, Version = SystemVersion }));
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint($"/swagger/{SystemVersion}/swagger.json", SystemName));
+        }
+    }
+}
